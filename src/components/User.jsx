fd25fe34a9524d9d3a30ffcc04030aacd0f82c63
ksamrat224 <ksamrat224@gmail.com>
2025-01-29 15:00:05 +0545

    class component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useState } from "react";
 
 const User =(props)=> {
+    const[count,setCount]=useState(0);
+    const[count1,setCount1]=useState(1);
     return (
      <div className="user-card">  
+        <h1>{count}</h1>
+        <h1>{count1}</h1>
         <h1>Name:{props.name}</h1>
         <h2>Location:{props.location}</h2>
         <h3>Contact:@samrat224(X)</h3>
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -3,11 +3,21 @@ import React from "react";
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+        this.state= {
+          count:0,
+          count1:1,};
     }
   render() {
     const {name,location}=this.props;
     return (
       <div className="user-card">
+        <h1>count:{this.state.count}</h1>
+        <button onClick={()=>{
+//Never update state variables directly
+this.setState({
+  count:this.state.count + 1,
+})
+        }}>Count Increase</button>
         <h1>Name:{name}</h1>
         <h2>Location: {location}</h2>
         <h3>Contact: @samrat224 (X)</h3>
